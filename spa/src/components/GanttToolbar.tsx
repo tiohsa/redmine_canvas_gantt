cd8d5ca3dc12feb8bb8386a16e66c26d97ab2606
glorydays <glorydays11@gmail.com>
2026-01-03 14:20:50 +0900

    PJフィルタアイコン修正

--- spa/src/components/GanttToolbar.tsx
+++ spa/src/components/GanttToolbar.tsx
@@ -539,7 +539,6 @@ export const GanttToolbar: React.FC<GanttToolbarProps> = ({ zoomLevel, onZoomCha
                     >
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-                            {groupByProject && <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" opacity="0.5" />}
                         </svg>
                         {i18n.t('label_project_short') || 'PJ'}
                     </button>
