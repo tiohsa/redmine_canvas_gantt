deceeb70cb34798f2cef7ee2343c39b782214d5b
tiohsa <49578425+tiohsa@users.noreply.github.com>
2026-07-16 19:31:57 +0900

    install same-origin session fetch

--- spa/src/main.tsx
+++ spa/src/main.tsx
@@ -16,9 +16,12 @@ import '@fontsource/roboto/700.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { installSameOriginSessionFetch } from './api/sessionFetch'
 import { SvgSpriteDefs } from './icons/SvgSpriteDefs'
 import './index.css'
 import './stores/preferencesWatcher'
+
+installSameOriginSessionFetch()
 
 const rootElement = document.getElementById('redmine-canvas-gantt-root') || document.getElementById('root');
 createRoot(rootElement!).render(
